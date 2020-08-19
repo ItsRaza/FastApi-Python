78adb92be7ad5eca916a13b8b5ab92f1e114fc1d
ItsRaza <razaabbas768@gmail.com>
2020-08-20 00:34:11 +0500

    commit

--- main.py
+++ main.py
@@ -1,11 +1,43 @@
-from fastapi import FastAPI
+from typing import List
+
+from fastapi import Depends, FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
+from starlette.responses import RedirectResponse
+
+from . import models, schemas
+from .database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-fake_items_db = [{"item_name": "Foo"}, {
-    "item_name": "Bar"}, {"item_name": "Baz"}]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
+
+# Dependency
 
 
-@app.get("/items/")
-async def read_item(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip: skip + limit]
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/")
+def main():
+    return RedirectResponse(url="/docs/")
+
+
+@app.get("/records/", response_model=List[schemas.Record])
+def show_records(db: Session = Depends(get_db)):
+    records = db.query(models.Record).all()
+    return records
