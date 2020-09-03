9f552ca19c92b412e24b2a82a85dbeb5b37421fe
ItsRaza <razaabbas768@gmail.com>
2020-09-03 21:54:03 +0500

    background process working okay

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,10 +1,38 @@
 
 
-from fastapi import FastAPI
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+
+# @app.get("/")
+# async def root():
+#     return {"message": "Tomato"}
+
+
+from typing import Optional
+
+from fastapi import BackgroundTasks, Depends, FastAPI
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Tomato"}
+def write_log(message: str):
+    with open("log.txt", mode="a") as log:
+        log.write(message)
+
+
+def get_query(background_tasks: BackgroundTasks, q: Optional[str] = None):
+    if q:
+        message = f"found query: {q}\n"
+        background_tasks.add_task(write_log, message)
+    return q
+
+
+@app.post("/send-notification/{email}")
+async def send_notification(
+    email: str, background_tasks: BackgroundTasks, q: str = Depends(get_query)
+):
+    message = f"message to {email}\n"
+    background_tasks.add_task(write_log, message)
+    return {"message": "Message sent"}
